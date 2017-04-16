d3ce52f5ff17c5250b1162584c6c65609eb5ba58
Jose Pena <josepena@MacBook-Pro-de-Jose.local>
2017-04-16 13:35:45 -0400

    uploading the api to github

---
+++ knightlife/src/java/knightlife/KnightLife.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knightlife;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+/**
+ * REST Web Service
+ *
+ * @author josepena
+ */
+@Path("rest")
+public class KnightLife {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of KnightLife
+     */
+    public KnightLife() {
+    }
+
+    /**
+     * Retrieves representation of an instance of webservice.KnightLife
+     * @return an instance of java.lang.String
+     */
+    
+    @GET
+    @Path("organizations")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRSO() throws FileNotFoundException, IOException, ParseException {
+        ArrayList<Organization> db = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+        JSONArray a = (JSONArray) parser.parse(new InputStreamReader(getClass().getResourceAsStream("RSO.json")));
+//        for (Object o : a) {
+//            JSONObject org = (JSONObject) o;
+//            Organization orga = new Organization();
+//            orga.owner = (String) org.get("owner");
+//            orga.name = (String) org.get("name");
+//            orga.description = (String) org.get("description");
+//            orga.image = (String) org.get("image");
+//            db.add(orga);
+//        }
+        
+       return Response
+            .status(200)
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+            .header("Access-Control-Allow-Credentials", "true")
+            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+            .header("Access-Control-Max-Age", "1209600")
+            .entity(a.toString())
+            .build();
+    }
+
+    @GET
+    @Path("events")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getEvents() throws FileNotFoundException, IOException, ParseException {
+        ArrayList<Event> db = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+        JSONArray a = (JSONArray) parser.parse(new InputStreamReader(getClass().getResourceAsStream("events.json")));
+//        for (Object o : a) {
+//            JSONObject ev = (JSONObject) o;
+//            Event event = new Event();
+//            event.date = (String) ev.get("date");
+//            event.eid = (String) ev.get("eid");
+//            event.dateend = (String) ev.get("dateend");
+//            event.title = (String) ev.get("title");
+//            event.owner = (String) ev.get("owner");
+//            event.tags = (ArrayList<String>) ev.get("tags");
+//             
+//            db.add(event);
+//        }
+        
+        return Response
+            .status(200)
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+            .header("Access-Control-Allow-Credentials", "true")
+            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+            .header("Access-Control-Max-Age", "1209600")
+            .entity(a.toString())
+            .build();
+    }
+}
---
+++ knightlife/src/java/knightlife/Organization.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knightlife;
+
+/**
+ *
+ * @author josepena
+ */
+public class Organization {
+    public String owner;
+    public String description;
+    public String image;
+    public String name;
+    
+}
