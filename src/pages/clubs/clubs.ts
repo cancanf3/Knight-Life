437ed62962c02bd3346807908af44a9fbe30655f
David <davidisrawi@gmail.com>
2017-04-17 18:28:42 -0400

    css on point

--- src/pages/clubs/clubs.ts
+++ src/pages/clubs/clubs.ts
@@ -12,23 +12,23 @@ export class ClubsPage {
 
     items: any[];
     clubs: Club[];
+    clubsInitial: Club[];
+    searching: String;
 
     constructor(public navCtrl: NavController, private orgService:OrgService) {
         this.clubs = [];
-        console.log("baby");
+        this.clubsInitial = [];
+        this.searching = '';
     }
 
     itemSelected(item) {
         this.navCtrl.push(ClubPage, {
             item: item,
         });
-        console.log('pushed item:');
-        console.log(item);
     }
 
     ngOnInit() {
         this.getOrgs();
-        console.log('mama');
     }
 
     getOrgs() {
@@ -46,9 +46,35 @@ export class ClubsPage {
             club.description = i.description;
             club.owner = i.owner;
             club.image = i.image;
+
             this.clubs.push(club);
+            this.clubsInitial.push(club);
         }
-        //console.log(this.clubs[0]);
     }
 
+    search(club) {
+        this.clubs = this.clubsInitial;
+        console.log("Parameter:");
+        console.log(club);
+        var value = club.path[0].value;
+
+        if(value == undefined) {
+            return;
+        }
+
+        // if the value is an empty string don't filter the items
+        if (value.trim() == '') {
+            return;
+        }
+
+        this.clubs = this.clubs.filter((v) => {
+            if(v.name.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                return true;
+            }
+            if(v.description.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                return true;
+            }
+            return false;
+        })
+    }
 }
