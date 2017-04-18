8c45915d6a0aa59948a02db18427ba1b4c331394
David <davidisrawi@gmail.com>
2017-04-18 01:07:18 -0400

    form resets after submitting

--- src/pages/submit-ev/submit-ev.ts
+++ src/pages/submit-ev/submit-ev.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 /*
-  Generated class for the SubmitEv page.
+Generated class for the SubmitEv page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
+See http://ionicframework.com/docs/v2/components/#navigation for more info on
+Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-submit-ev',
-  templateUrl: 'submit-ev.html'
+    selector: 'page-submit-ev',
+    templateUrl: 'submit-ev.html'
 })
 export class SubmitEvPage {
     title: String;
@@ -17,28 +17,28 @@ export class SubmitEvPage {
     location: String;
     date: Date;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
-      this.title = '';
-      this.description = '';
-      this.location = '';
-      this.date = new Date();
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+        this.title = '';
+        this.description = '';
+        this.location = '';
+        this.date = new Date();
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SubmitEvPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SubmitEvPage');
+    }
 
-  showAlert() {
-   let alert = this.alertCtrl.create({
-     title: 'Request submitted!',
-     subTitle: 'Your request has been sent to a SGA representative, once it\'s approved, it\'ll be added to the events list',
-     buttons: ['OK']
-   });
-   this.title = '';
-   this.description = '';
-   this.location = '';
-   this.date = new Date();
-   alert.present();
- }
+    showAlert() {
+        let alert = this.alertCtrl.create({
+            title: 'Request submitted!',
+            subTitle: 'Your request has been sent to a SGA representative, once it\'s approved, it\'ll be added to the events list',
+            buttons: ['OK']
+        });
+        this.title = '';
+        this.description = '';
+        this.location = '';
+        this.date = new Date();
+        alert.present();
+    }
 
 }
